182c7125d43d52bc8ae6f6d807640acab6b6cbee
Guto Bertagnolli <pomerode@gmail.com>
2026-08-10 13:38:45 -0300

    enforce authentication globally

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,23 +1,25 @@
-import { SubstitutionsModule } from './substitutions/substitutions.module';
-import { AbsencesModule } from './absences/absences.module';
-import { AvailabilityModule } from './availability/availability.module';
-import { EmployeeWeeklySchedulesModule } from './employee-weekly-schedules/employee-weekly-schedules.module';
-import { EmployeesModule } from './employees/employees.module';
-import { ClassesModule } from './classes/classes.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './common/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { TenantsModule } from './tenants/tenants.module';
-import { UsersModule } from './users/users.module';
-import { SchoolsModule } from './schools/schools.module';
-import { TimeTemplatesModule } from './time-templates/time-templates.module';
-import { SettingsModule } from './settings/settings.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AbsencesModule } from './absences/absences.module';
 import { AnnouncementsModule } from './announcements/announcements.module';
-import { SubjectsModule } from './subjects/subjects.module';
 import { AuditLogsModule } from './audit-logs/audit-logs.module';
-import { WhatsAppModule } from './whatsapp/whatsapp.module';
+import { AuthGuard } from './auth/auth.guard';
+import { AuthModule } from './auth/auth.module';
+import { AvailabilityModule } from './availability/availability.module';
+import { ClassesModule } from './classes/classes.module';
+import { PrismaModule } from './common/prisma.module';
 import { DashboardModule } from './dashboard/dashboard.module';
+import { EmployeeWeeklySchedulesModule } from './employee-weekly-schedules/employee-weekly-schedules.module';
+import { EmployeesModule } from './employees/employees.module';
+import { SchoolsModule } from './schools/schools.module';
+import { SettingsModule } from './settings/settings.module';
+import { SubjectsModule } from './subjects/subjects.module';
+import { SubstitutionsModule } from './substitutions/substitutions.module';
+import { TenantsModule } from './tenants/tenants.module';
+import { TimeTemplatesModule } from './time-templates/time-templates.module';
+import { UsersModule } from './users/users.module';
+import { WhatsAppModule } from './whatsapp/whatsapp.module';
 
 @Module({
   imports: [
@@ -40,6 +42,12 @@ import { DashboardModule } from './dashboard/dashboard.module';
     TimeTemplatesModule,
     EmployeesModule,
     WhatsAppModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
 })
 export class AppModule {}
